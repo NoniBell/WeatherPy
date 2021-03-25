bb283b42eb40f120762c986d3b9c12128c1c2eb6
Noni Bell <nonibell2020@u.northwestern.edu>
2021-03-25 11:35:37 -0500

    added output data file

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "67b1fe23502ecd634359a2c7534cb95d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA21HhSM7vS08M94kvz6iJxsz_j9WlwS9Y"
